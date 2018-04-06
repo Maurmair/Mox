43d85b79620dbb2657f244dc9101f16ff672a6e9
Maurmair <maurmair@gmail.com>
2018-04-06 13:12:14 +0200

    Get request redirected

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-device-name.DeviceName",
+    "file": "plugins/cordova-plugin-device-name/www/device-name.js",
+    "pluginId": "cordova-plugin-device-name",
+    "clobbers": [
+      "cordova.plugins.deviceName"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-device-name": "1.3.2"
+};
+// BOTTOM OF METADATA
+});
