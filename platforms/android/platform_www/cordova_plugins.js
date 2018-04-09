88f705205773034957a4f7c181cb661de6b6f5c2
Wesley Maurmair <maurmair@gmail.com>
2018-04-09 21:16:12 +0200

    Poging tot kleurschema

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
     "id": "cordova-plugin-device-name.DeviceName",
     "file": "plugins/cordova-plugin-device-name/www/device-name.js",
     "pluginId": "cordova-plugin-device-name",
@@ -12,6 +20,7 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+  "cordova-plugin-device": "2.0.1",
   "cordova-plugin-device-name": "1.3.2"
 };
 // BOTTOM OF METADATA
