0d7c928adcc6a79584648237d2d376d863c387e2
Wesley Maurmair <maurmair@gmail.com>
2018-04-11 17:17:16 +0200

    added cordova-diagnostic plugin

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -2,6 +2,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova.plugins.diagnostic": "4.0.5"
+}
 // BOTTOM OF METADATA
 });
