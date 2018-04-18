449ecb47ef605dd11f3e640db2b0a1e7a45982b5
Maurmair <maurmair@gmail.com>
2018-04-18 16:41:24 +0200

    updated Progress handling

--- platforms/ios/www/js/handleSettingsScreen.js
+++ platforms/ios/www/js/handleSettingsScreen.js
@@ -21,24 +21,32 @@ window.onload = function () {
 function addToInputMinutes() {
     myTarget.ActiveMinutes = myTarget.ActiveMinutes + 50;
     document.getElementById("activeMinutesInput").value = myTarget.ActiveMinutes;
+    document.getElementById("toonDoelActieveMinuten").innerHTML = myTarget.ActiveMinutes;   
+    updateProgressBar();   
     console.log(myTarget);
 }
 
 function substractFromInputMinutes() {
     myTarget.ActiveMinutes = myTarget.ActiveMinutes - 50;
     document.getElementById("activeMinutesInput").value = myTarget.ActiveMinutes;
+    document.getElementById("toonDoelActieveMinuten").innerHTML = myTarget.ActiveMinutes; 
+    updateProgressBar();  
     console.log(myTarget);
 }
 
 function addToInputSteps() {
     myTarget.Steps = myTarget.Steps + 50;
     document.getElementById("stepsInput").value = myTarget.Steps;
+    document.getElementById("toonDoelStappen").innerHTML = myTarget.Steps;
+    updateProgressBar();  
     console.log(myTarget);
 }
 
 function substractFromInputSteps() {
     myTarget.Steps = myTarget.Steps - 50;
     document.getElementById("stepsInput").value = myTarget.Steps;
+    document.getElementById("toonDoelStappen").innerHTML = myTarget.Steps;
+    updateProgressBar();  
     console.log(myTarget);
 }
 
@@ -48,6 +56,8 @@ function pushtoDB() {
     // myTarget.DeviceId = "GVD";
     // myTarget.DeviceId = deviceId;
     console.log(myTarget);
+    updateProgressBar();  
     makeTheCall();
+    
     // window.location.reload()
 }
